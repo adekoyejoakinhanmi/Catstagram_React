08933e714bc050b37bfa694317bcab058f305ee3
Adekoyejo Akinhanmi <koyesavedbygrace@yahoo.com>
2019-01-14 18:15:26 +0100

    use CatList component in App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
+import React from 'react'
+import { CatList } from "./components"
+class App extends React.Component {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    <div className="container">
+      <h1>Catstagram</h1>
+      <CatList/>
+    </div>)
   }
 }
 
